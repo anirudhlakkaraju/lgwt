a06b62dfbd42594336d54bf3ee50a2858ab2b75b
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-01-30 21:10:50 -0600

    test: sum slice of ints

---
+++ arrays/sum_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	sum := Sum(numbers)
+	expected := 15
+
+	if sum != expected {
+		t.Fatalf("want %d, got %d, given %v", expected, sum, numbers)
+	}
+
+}
