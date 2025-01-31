d603dc3c4d7de52839ad7ae295a080367c5bed24
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-01-30 21:50:50 -0600

    refactor: assertion check

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -29,21 +29,21 @@ func TestSumAll(t *testing.T) {
 
 func TestSumAllTails(t *testing.T) {
 
-	t.Run("Input slices with values", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 11})
-		want := []int{5, 20}
-
+	checkSum := func(t testing.TB, got, want []int) {
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("want %d, got %d", want, got)
 		}
+	}
+
+	t.Run("Input slices with values", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 11})
+		want := []int{5, 20}
+		checkSum(t, got, want)
 	})
 
 	t.Run("Input slices with no values", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{0, 9, 11})
 		want := []int{0, 20}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("want %d, got %d", want, got)
-		}
+		checkSum(t, got, want)
 	})
 }
