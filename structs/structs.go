3f8ea5031de8d19c14c103f8ead3cb1345dcee2e
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-03 20:44:06 -0600

    feat: use shape interface for further abstraction

--- structs/structs.go
+++ structs/structs.go
@@ -2,12 +2,16 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func (r *Rectangle) Area() float64 {
+func (r Rectangle) Area() float64 {
 	return r.height * r.width
 }
 
@@ -15,7 +19,7 @@ type Circle struct {
 	radius float64
 }
 
-func (c *Circle) Area() float64 {
+func (c Circle) Area() float64 {
 	return math.Pi * (c.radius * c.radius)
 }
 
