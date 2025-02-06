3f1755c19f525870b2c403dfdf307308b69d4851
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-05 22:01:58 -0600

    feat: handle addition of existing word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find word you were looking for")
+var ErrWordExists = errors.New("word already exists in dictionary")
 
 func (d Dictionary) Search(key string) (string, error) {
 	value, ok := d[key]
@@ -15,6 +16,11 @@ func (d Dictionary) Search(key string) (string, error) {
 	return value, nil
 }
 
-func (d Dictionary) Add(key, value string) {
-	d[key] = value
+func (d Dictionary) Add(key, value string) error {
+	_, ok := d[key]
+	if !ok {
+		d[key] = value
+		return nil
+	}
+	return ErrWordExists
 }
