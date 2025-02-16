7dcdef0e9aeef3aa4ff5ff8aa4c4bbe2f96e1154
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-15 21:25:44 -0600

    refactor: use select to race concurrent pings

--- select/select.go
+++ select/select.go
@@ -2,21 +2,28 @@ package sel
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := getResponseTime(a)
-	bDuration := getResponseTime(b)
-
-	if aDuration < bDuration {
+	// 	Waiting for values to be sent to a channel with myVar := <-ch is a blocking call
+	// `select` allows you to wait on multiple channels. The first one to send a value "wins" and the code underneath the case is executed.
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func getResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+// - A `chan struct{}` is the smallest data type available from a memory perspective so we get no memory allocation
+// - Always **make** channels!
+// - Rather than say `var ch chan struct{}`, when you use `var` the variable will be initialised with the "zero" value of the type. So for `string` it is `""`, `int` it is `0`, etc.
+//   - For `channels` the zero value is `nil` and if you try and send to it with `<-` it will block forever because you cannot send to `nil` channels
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
