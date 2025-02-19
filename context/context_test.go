de03c342ef0639321f5d3e756d45ab80883bee0b
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-18 22:02:44 -0600

    feat: happy path for fetching data

---
+++ context/context_test.go
@@ -0,0 +1,37 @@
+package context
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() bool {
+	return s.cancelled
+}
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	cancelled := false
+	svr := Server(&SpyStore{data, cancelled})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+	}
+}
