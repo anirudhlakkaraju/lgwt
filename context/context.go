78a20154806c2d7be414837ee9903ce97dd79610
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-19 21:41:06 -0600

    feat: handle response and done signals gracefully

--- context/context.go
+++ context/context.go
@@ -12,7 +12,29 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Fprint(w, store.Fetch())
+
+		// context has a method Done() which returns a channel
+		// which gets sent a signal when the context is "done" or "cancelled".
+		// We want to listen to that signal and call store.Cancel if we get it
+		// but we want to ignore it if our Store manages to Fetch before it.
+		//
+		// To manage this we run Fetch in a goroutine and it will write the result into a new channel data.
+		// We then use select to effectively race to the two asynchronous processes
+		// and then we either write a response or Cancel.
+
+		ctx := r.Context()
+
+		data := make(chan string)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
