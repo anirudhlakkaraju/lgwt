9538f1607ac5f2e3d62455fbb4a8ce2727b2f8f4
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-19 21:28:11 -0600

    feat: test cancel when timeout

--- context/context_test.go
+++ context/context_test.go
@@ -1,6 +1,7 @@
 package context
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -17,21 +18,42 @@ func (s *SpyStore) Fetch() string {
 	return s.response
 }
 
-func (s *SpyStore) Cancel() bool {
-	return s.cancelled
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func TestServer(t *testing.T) {
-	data := "hello, world"
-	cancelled := false
-	svr := Server(&SpyStore{data, cancelled})
+	t.Run("happy path", func(t *testing.T) {
+		data := "hello, world"
+		svr := Server(&SpyStore{response: data})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
 
-	svr.ServeHTTP(response, request)
+		svr.ServeHTTP(response, request)
 
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-	}
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("store was told not to cancel")
+		}
+	})
 }
