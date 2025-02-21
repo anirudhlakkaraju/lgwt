1a05f03eb0788583d8ac29bcc416d96dcd398064
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-20 20:36:37 -0600

    refactor: table format test cases

--- roman_numerals/roman_numerals_test.go
+++ roman_numerals/roman_numerals_test.go
@@ -3,21 +3,21 @@ package romannumerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("convert 1 to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	tests := []struct {
+		Description string
+		Number      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
-	t.Run("convert 2 to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	for _, test := range tests {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Number)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
 }
